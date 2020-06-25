b5128af455cdc23abd3485a02b6d8721def31b4a
konductor000 <59878172+konductor000@users.noreply.github.com>
2020-06-25 22:52:44 +0300

    работает без коментариев

--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Article, Comment
+from django.http import Http404, HttpResponseRedirect
 
 def index(request):
-	return render(request, 'articles/list.html')
+	latest_articles_list = Article.objects.order_by('-pub_date')[:5]
+	return render(request, 'articles/list.html', {'latest_articles_list' : latest_articles_list})
 
+def detail(request, article_id):
+	try:
+		a = Article.objects.get( id = article_id )
+	except:
+		raise Http404("Статья не найдена")
+
+	return render(request, 'articles/detail.html', {'article' : a})
